7e3c591f87a3591320f4dfa4393ec46599644d60
Kutalmis Bercin <kutalmis.bercin@keysight.com>
2026-03-12 16:13:30 +0000

    performanceMetric: set predetermined problematic processor weights

--- src/dynamicLoadBalancing/performanceMetrics/performanceMetric/performanceMetric.C
+++ src/dynamicLoadBalancing/performanceMetrics/performanceMetric/performanceMetric.C
@@ -195,14 +195,14 @@ Foam::tmp<Foam::scalarField> Foam::performanceMetric::processorWeights() const
     // Already reduced imbalance values - MOVED?
     // prevImbalanceValue_ = imbalanceValue_;
 
-    /*
+
     auto tweightsTest = tmp<scalarField>::New(nProcs, scalar(0));
     scalarField& weightsTest = tweightsTest.ref();
-    weightsTest[0] = 1;
-    weightsTest[1] = 2;
+    weightsTest[0] = 998959;  // problematic processor weights
+    weightsTest[1] = 1041;
 
-    return tweightsTest;*/
-    return tweights;
+    return tweightsTest;
+//    return tweights;
 }
 
 
